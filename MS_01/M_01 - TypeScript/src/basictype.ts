951a4642661b793c89e087d23e5d427507261310
abushamacd <assiddik001@gmail.com>
2024-07-28 15:59:10 +0600

    m1 v4 - Primitive and non-primitive

---
+++ MS_01/M_01 - TypeScript/src/basictype.ts
@@ -0,0 +1,16 @@
+// Primitive
+let userName: string = "Abu Shama";
+
+let age: number = 25;
+
+let marritialStatus: boolean = true;
+
+let hasBaby: null = null;
+
+let babyGender: undefined = undefined;
+
+// Non primitive
+
+let frinds: [string, number] = ["Abu Shama", 23];
+
+let numberList: number[] = [12, 324, 231, 23];
